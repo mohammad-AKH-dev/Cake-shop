6ee5b60bc3f9294fa0ab90368fcda572ed6f5e81
Mohammad (FrontEnd Developer) <mohammadakhlaghi843@gmail.com>
2025-04-24 23:29:11 +0330

    search products box responsive now

--- src/components/modules/shop/SearchedProduct.tsx
+++ src/components/modules/shop/SearchedProduct.tsx
@@ -17,14 +17,14 @@ function SearchedProduct(props: productBoxType) {
   return (
     <Link
       href={`/product/${title}/${id}`}
-      className="searched-product flex w-full bg-new-products py-3 max-h-[70px] items-center justify-between px-4 border-b border-b-[#e1e1e1]"
+      className="searched-product text-[12px] flex gap-x-4 w-full bg-new-products py-3 max-h-[70px] items-center justify-start md:justify-between px-4 border-b border-b-[#e1e1e1]"
     >
-      <div className="image-wrapper w-[60px] h-[60px] flex items-center justify-center my-auto -mt-4">
+      <div className="image-wrapper w-[70px] h-[70px] md:w-[45px] md:h-[45px] lg:w-[60px] lg:h-[60px] flex items-center justify-center my-auto -mt-4">
         <Image src={sources[0]} width={400} height={400} alt={title} className="w-full h-full flex items-center justify-center"/>
       </div>
-      <div className="searched-product__infos text-left min-w-[150px]">
-        <h4 className="searched-product__title font-bold">{title}</h4>
-        <div className="searched-product__prices flex items-center gap-x-3">
+      <div className="searched-product__infos md:mx-auto md:max-w-[80px] lg:text-left overflow-hidden flex flex-col text-ellipsis lg:min-w-[150px]">
+        <h4 className="searched-product__title font-bold overflow-hidden whitespace-nowrap text-ellipsis">{title}</h4>
+        <div className="searched-product__prices flex flex-wrap lg:flex-nowrap items-center gap-x-3">
           {discount ? (
             <>
               <span className="price line-through text-text opacity-50 font-bold">
